b77fe1502cf678c2877b062517f6af894cf0cbee
pat2echo <pat2echo@gmail.com>
2024-05-17 01:11:15 +0000

    Bump version: 0.1.0 → 0.2.0

--- ai_project_setup_package/setup.py
+++ ai_project_setup_package/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_project_setup',
-    version='3.1.0',
+    version='0.2.0',
     packages=find_packages(),
-    description='A utility package for setting up AI project structures.',
+    description='A versatile utility package designed to streamline the setup of AI project structures while seamlessly integrating with Git repositories and Google Colab, simplifying the process of building collaborative AI projects.',
     author='Patrick Ogbuitepu',
     author_email='pat2echo@gmail.com',
-    keywords=['project', 'setup', 'structure', 'ai', 'artificial intelligence', 'research', 'data science', 'machine learning'],
+    keywords=['AI', 'utility package', 'project structure', 'Git integration', 'Google Colab integration', 'collaborative projects', 'ease of use', 'streamlined workflow', 'automation', 'project management', 'project', 'setup', 'structure', 'ai', 'artificial intelligence', 'research', 'data science', 'machine learning'],
     install_requires=[
         # Any dependencies the package might have, e.g.,
         # 'numpy', 'pandas'
