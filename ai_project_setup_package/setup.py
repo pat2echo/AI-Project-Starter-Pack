a14ec1e882d991bd7336cec2329adc90c5738ffb
pat2echo <pat2echo@gmail.com>
2024-05-17 01:05:08 +0000

    Bump version: 1.0.0 → 2.0.0

--- ai_project_setup_package/setup.py
+++ ai_project_setup_package/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_project_setup',
-    version='0.1.0',
+    version='2.0.0',
     packages=find_packages(),
     description='A utility package for setting up AI project structures.',
     author='Patrick Ogbuitepu',
